e99a5d9e334782455d82326ad0888c7b03d22fe0
root <root@vps1089.directvps.nl>
2021-09-09 13:37:42 +0200

    changed welcome message once more

--- test1/Hello.java
+++ test1/Hello.java
@@ -2,8 +2,8 @@ public class Hello {
 
     public static void main(String[] args) {
     
-        for (int i=1; i<=10; i++) {
-            System.out.println("Howdy World ... " + i + "!");
+        for (int i=1; i<=15; i++) {
+            System.out.println("How'd you do World ... " + i + "?");
         }
     }
 
