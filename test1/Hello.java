c1cc91fbcb99373f285891617977c5275b13ff1e
acdhirr <acdhirr@trilobiet.nl>
2021-09-09 14:27:22 +0200

    change made by Grover

--- test1/Hello.java
+++ test1/Hello.java
@@ -3,7 +3,7 @@ public class Hello {
     public static void main(String[] args) {
     
         for (int i=1; i<=15; i++) {
-            System.out.println("How'd you do World ... " + i + "?");
+            System.out.println("Grover sez: How'd you do World ... " + i + "?");
         }
     }
 
