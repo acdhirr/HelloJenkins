79b155de2b5e75a408527c54e77f42fffdee2e43
root <root@vps1089.directvps.nl>
2021-09-22 14:52:19 +0200

    burp2

--- test1/testscript.sh
+++ test1/testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "And now for something completely different..."
+echo "This -burp- is something completely different..."
 
